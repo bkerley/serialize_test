a8a1c181a7304cacffe2cfd493934b4f27b1be98
Bryce Kerley <bryce@worldmedia.net>
2008-07-15 14:16:21 -0400

    check to see if saving screws it up

--- test/unit/hat_test.rb
+++ test/unit/hat_test.rb
@@ -5,7 +5,8 @@ class HatTest < ActiveSupport::TestCase
     description = <<EOF
 <span style="color: #800">SODA HAT IS A SLANG TERM FOR A LID</span>
 EOF
-    h = Hat.create(:name=>'Soda Hat', :description=>description)
+    h = Hat.new(:name=>'Soda Hat', :description=>description)
+    assert_equal description, h.description
     h.save
     assert_equal description, h.description
   end
